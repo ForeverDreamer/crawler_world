5f61c3f1a252a4a00d36f06392ef2c585feecdac
doer <499361328@qq.com>
2019-05-13 13:17:50 +0800

    requests和Chrome Webdriver的cookies设置和获取

--- proxy_pool/Run/headless_chrome_test.py
+++ proxy_pool/Run/headless_chrome_test.py
@@ -23,32 +23,39 @@ cookie_dict = {
     '__utmv':r'51854390.100-1|2=registration_date=20121016=1^3=entry_date=20121016=1'
 }
 
+URL = "http://www.baidu.com"
+# URL = "http://crawleruniverse.com:8000/ct/ri"
+
 try:
     # 设置参数选项
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     proxy_ip = "186.24.11.165:8080"
     user_agent = random.choice(ua_list)
-    chrome_options.add_argument('--proxy-server=' + proxy_ip)
+    # chrome_options.add_argument('--proxy-server=' + proxy_ip)
     chrome_options.add_argument("--user-agent=" + user_agent)
-
     driver = webdriver.Chrome(options=chrome_options)
+
+    # 应该不需要设置cookies，本身就是浏览器，访问网站时服务器写入的cookies都保存在Chrome Webdriver进程中，
+    # 可以通过driver.get_cookies()获取，再次访问该网站时也会自动发送给服务器
     # for name, value in cookie_dict.items():
     #     driver.add_cookie({'name': name, 'value': value})
 
-    driver.get("http://crawleruniverse.com:8000/ct/ri")
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, features='lxml')
-
-    http_x_forwarded_for = re.findall(r'\d+.\d+.\d+.\d+', str(soup.find("h4")))
-    remote_addr = re.findall(r'\d+.\d+.\d+.\d+', str(soup.find("h5")))[0]
-    proxy_ip = proxy_ip.split(':')[0]
-    print('http_x_forwarded_for: {}, remote_addr: {}, proxy_ip: {}, pass: {}'.format(http_x_forwarded_for,
-                                                                                     remote_addr,
-                                                                                     proxy_ip,
-                                                                                     proxy_ip == remote_addr))
-    print(soup.find('div').get_text())
-    print(soup.find('span').get_text())
+    driver.get(URL)
+    # page_source = driver.page_source
+    # soup = BeautifulSoup(page_source, features='lxml')
+    #
+    # http_x_forwarded_for = re.findall(r'\d+.\d+.\d+.\d+', str(soup.find("h4")))
+    # remote_addr = re.findall(r'\d+.\d+.\d+.\d+', str(soup.find("h5")))[0]
+    # proxy_ip = proxy_ip.split(':')[0]
+    # print('http_x_forwarded_for: {}, remote_addr: {}, proxy_ip: {}, pass: {}'.format(http_x_forwarded_for,
+    #                                                                                  remote_addr,
+    #                                                                                  proxy_ip,
+    #                                                                                  proxy_ip == remote_addr))
+    # print(soup.find('div').get_text())
+    # print(soup.find('span').get_text())
+    print('response cookies: ')
+    print(driver.get_cookies())
 except Exception as e:
     traceback.print_exc()
 finally:
