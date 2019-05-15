27f63c4da0b0fb28f8ffdb230277ae47dc4dc3e9
doer <499361328@qq.com>
2019-05-15 09:02:37 +0800

    重构user_agent转换函数，支持目录递归转换

--- proxy_pool/Util/convert_to_ua.py
+++ proxy_pool/Util/convert_to_ua.py
@@ -1,10 +1,28 @@
-ua_list = []
-try:
-    with open('chrome.txt', 'r') as f:
-        for line in f.readlines():
-            ua_list.append("\'" + line.strip() + "\'" + ',' + '\n')
+import os
 
-    with open('ua_list.txt', 'w') as f:
-        f.writelines(ua_list)
-except Exception as e:
-    print(e)
+ROOT_DIR = 'user_agent_list'
+TARGET_FILE = 'user_agents.py'
+
+with open(TARGET_FILE, 'at') as fw:
+    fw.write('ua_list = [\n')
+
+for cur_dir_path, sub_dirs, cur_dir_files in os.walk(ROOT_DIR):
+    for file in cur_dir_files:
+        # 过滤蜘蛛user_agent
+        if file.endswith(".txt"):
+            # 两种拼接文件路径的方式效果一样
+            # print(os.path.join(cur_dir_path, file))
+            # print(cur_dir_path + os.sep + file)
+            file_path = os.path.join(cur_dir_path, file)
+            try:
+                with open(file_path, 'rt') as fr:
+                    with open(TARGET_FILE, 'at') as fw:
+                        for line in fr.readlines():
+                            fw.write("\t\'" + line.strip() + "\'\n")
+            except Exception as e:
+                print(e)
+
+
+with open(TARGET_FILE, 'at') as fw:
+    fw.write(']\n')
+
